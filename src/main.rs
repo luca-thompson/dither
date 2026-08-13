2b700cfb8a428dce4ac608c4e2849c41614b708b
luca-thompson <lucathompson006@gmail.com>
2026-08-13 23:54:35 +1000

    basic image saving working

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
+//args
 use clap::Parser;
 
-#[derive(Parser, Debug)]
+//image
+use image::ImageReader;
 
+
+#[derive(Parser, Debug)]
 struct Args {
 
-    #[arg(short, long)]
-    name: String,
+    #[arg(long)]
+    f_in: String,
+
+    #[arg(long)]
+    f_out: String
 
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     let args = Args::parse();
 
-    println!("{}", args.name);
+    println!("{}", args.f_in);
+    println!("{}", args.f_out);
+
+    let img = ImageReader::open(args.f_in)?.decode()?;
+
+    img.save(args.f_out)?;
+
+    Ok(())
 }

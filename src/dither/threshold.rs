c38a08b84500c2721be370e15be18436dadb8c71
luca-thompson <lucathompson006@gmail.com>
2026-08-17 12:26:47 +1000

    basic threshold support

--- src/dither/threshold.rs
+++ src/dither/threshold.rs
@@ -1,6 +1,24 @@
-use image::DynamicImage;
+use image::{DynamicImage, GenericImage, GenericImageView};
 
 pub fn threshold_dither(image: &mut DynamicImage){
-    
+    let (width, height) = image.dimensions();
+
+    const BLACK: image::Rgba<u8> = image::Rgba([0,0,0,255]);
+
+    const WHITE: image::Rgba<u8> = image::Rgba([255,255,255,255]);
+
+    for x in 0..width{
+        for y in 0..height{
+            if image.get_pixel(x, y)[1] < 128{
+                image.put_pixel(x, y, BLACK);
+            }
+            else{
+                image.put_pixel(x, y, WHITE);
+            }
+            
+        }
+
+        
+    }
     
 }

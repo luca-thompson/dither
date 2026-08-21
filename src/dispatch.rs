6976ef0d406620bc1f32c939cd7c25c802f4e95c
luca-thompson <lucathompson006@gmail.com>
2026-08-21 13:50:54 +1000

    added grayscale as an algorithm option

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -2,6 +2,7 @@
 #[path = "dither/bayer.rs"] mod bayer;
 #[path = "dither/halftone.rs"] mod halftone;
 #[path = "dither/random.rs"] mod random;
+#[path = "grayscale.rs"] mod grayscale;
 
 use image::DynamicImage;
 
@@ -11,6 +12,9 @@ pub fn dispatch(image: &mut DynamicImage, algorithm: String){
     let alg = algorithm.as_str();
 
     match alg {
+        "grayscale" => {
+            grayscale::grayscale(image);
+        }
         "threshold" => {
             threshold::threshold_dither(image);
         }
